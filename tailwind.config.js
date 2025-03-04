69def869a4547c4dd0197252f72a0cd3b5d9646c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-03-04 21:25:59 +0000

    Fix deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          750: '#2D3748', // Custom color between gray-700 and gray-800
+        },
+      },
+    },
   },
   plugins: [],
 };
